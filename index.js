ff41c84d6468fd24645b9dc398c91c28231963fc
PrateekTechmatys <prateeky562@gmail.com>
2024-03-17 17:15:45 +0530

    header is done

---
+++ index.js
@@ -0,0 +1,88 @@
+let search = document.getElementById('search');
+let btn = document.getElementById('btn1');
+let main = document.getElementById('main1');
+console.log(btn,search,main);
+let phones= [];
+
+btn.addEventListener('click', function() {
+   let a = search.value;
+//    console.log(a);
+   fetchsearch(a);
+});
+
+async function fetchsearch(a) {
+  try{
+    let div = document.createElement('div');
+    if(a.length > 0){
+        main.innerHTML="";
+        
+        const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${a}`);
+    // console.log(response);
+    const data = await response.json();
+    phones = data.data;
+    console.log(phones);
+
+   
+    phones.forEach((phone)=>{
+        div.innerHTML += `
+        <div class="card">
+        <div class="card-body">
+        <img src=${phone.image} alt="noShow"/>
+        <h5 class="card-title">${phone.brand}</h5>
+        <p class="card-text">${phone.phone_name}</p>
+        <p class="card-text">${phone.price}</p>
+        </div>
+        </div>
+        `;
+        
+    })
+    div.classList.add('card-front');
+    }
+    else if(a.length ==0) {
+        alert('Please type to search');
+        main.innerHTML="";
+        div.innerHTML = `<div class"no_results">
+        <h1>No Results Found</h1>
+        <button class="btn">Show More</button>
+        </div>`;
+        phones=[];
+        div.classList.add('no_resultsdiv');
+    }
+    phones=[];
+    
+    main.appendChild(div);
+  }
+  catch(err){
+    console.log(err);
+  }
+    
+}
+
+
+window.onload = async function(){
+    let div = document.createElement('div');
+    main.innerHTML="";
+        
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+// console.log(response);
+const data = await response.json();
+phones = data.data;
+console.log(phones);
+
+
+phones.forEach((phone)=>{
+    div.innerHTML += `
+    <div class="card">
+    <div class="card-body">
+    <img src=${phone.image} alt="noShow"/>
+    <h5 class="card-title">${phone.brand}</h5>
+    <p class="card-text">${phone.phone_name}</p>
+    <p class="card-text">${phone.price}</p>
+    </div>
+    </div>
+    `;
+    
+})
+div.classList.add('card-front');
+main.appendChild(div);
+}
